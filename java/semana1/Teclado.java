e24bbe51fabb05fd37ece3c797249d032f115d4b
Micaela <72576818+micaela2020-01@users.noreply.github.com>
2023-04-25 21:49:19 -0300

    1.4 Teclado

---
+++ java/semana1/Teclado.java
@@ -0,0 +1,17 @@
+package mundopc;
+
+public class Teclado extends DispositivoEntrada {
+
+    private final int idTeclado;
+    private static int contadorTeclados;
+
+    public Teclado(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        this.idTeclado = ++Teclado.contadorTeclados;
+    }
+
+    @Override
+    public String toString() {
+        return "Teclado(" + "idTeclado" + idTeclado + ", " + super.toString() + ')';
+    }
+}
