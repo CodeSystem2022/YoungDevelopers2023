90744c025b4b4c6cfcd8e8a19bb545cfd6733f54
lurebolledo <112673733+lurebolledo@users.noreply.github.com>
2023-05-29 21:45:23 -0300

    Update catalogo_peliculas.py

--- python/semana3/Dominio/catalogo_peliculas.py
+++ python/semana3/Dominio/catalogo_peliculas.py
@@ -1,21 +1,24 @@
-import os
-
-class CatalogoPeliculas:
-
-    ruta_archivo = 'peliculas.txt'
-
-    @classmethod
-    def agregar_peliculas(cls, pelicula):
-        with open(cls.ruta_archivo, 'a', encoding='utf8') as archivo:
-            archivo.write(f'{pelicula.nombre}\n')
-
-    @classmethod
-    def listar_peliculas(cls):
-        with open(cls.ruta_archivo, 'r', encoding='utf8') as archivo:
-            print(f'Catalogo de peliculas'.center(50, '-'))
-            print(archivo.read())
-
-    @classmethod
-    def eliminar_peliculas(cls):
-        os.remove(cls.ruta_archivo)
-        print(f'Archivo eliminado: {cls.ruta_archivo}')
+from dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+opcion = None
+while opcion != 4:
+    try:
+        print('Opciones: ')
+        print('1. Agregar película')
+        print('2. Lista de películas')
+        print('3. Eliminar catálogo de películas')
+        print('4. Salir')
+        opcion = int(input('Digite una opción de menú (1-4): '))
+        if opcion == 1:
+            nombre_pelicula = input('Digite el nombre de la película: ')
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
+    except Exception as e:
+        print(f'Ocurrió un error de tipo: {e}')
+        opcion = None
+    else:
+        print('Salimos del programa')
