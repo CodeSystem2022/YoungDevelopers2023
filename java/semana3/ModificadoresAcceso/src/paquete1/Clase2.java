fa5533d096a2bac53f24e0f7cc39ca53481fa43b
Ana Zambrana <anazambrana869@gmail.com>
2023-06-01 23:41:28 -0300

    Semana-3 Java

---
+++ java/semana3/ModificadoresAcceso/src/paquete1/Clase2.java
@@ -0,0 +1,15 @@
+
+package paquete1;
+
+
+class Clase2 {
+    String atributoDefault = "Valor del atributo default"; 
+    
+    Clase2(){
+        System.out.println("Constructor Default");
+    }
+    void metodoDefault(){
+        System.out.println("Metodo Default");
+    }
+    
+}
