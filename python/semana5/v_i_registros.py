15528ec256bba5d9313813fe47171113289b8802
agusces0 <agusces0@gmail.com>
2023-06-08 00:57:17 -0300

    Python clase 5 parte 2

---
+++ python/semana5/v_i_registros.py
@@ -0,0 +1,23 @@
+import psycopg2 # Esto es para poder conectarnos a Postgre
+
+conexion = psycopg2.connect(user = 'postgres', password = 'admin', host = '127.0.0.1', port = '5432', database='test_basedatos',
+)
+try:
+     with conexion:
+         with conexion.cursor() as cursor:
+             sentencia = 'INSERT INTO persona (nombre, apellido, email)VALUES (%s, %s, %s)'
+             valores =(
+                 ('Carlos', 'Lara', 'clara@gmail.com'),
+                 ('Marcos', 'Canto','mcanto@gmail.com'),
+                 ('Marcelo','Cuenca', 'cuencaM@gmail.com')
+
+             ) # Es un tupla de tuplas
+             cursor.executemany(sentencia, valores) # De esta manera ejecutamos la sentencia
+             #conexion.commit() # esto se utiliza para guardar los cambios en base de datos
+             registros_insertados = cursor.rowcount
+             print(f'Los registros insertados son :{registros_insertados}')
+
+except Exception as e:
+    print(f'Ocurrio un errror :{e}')
+finally:
+    conexion.close()
