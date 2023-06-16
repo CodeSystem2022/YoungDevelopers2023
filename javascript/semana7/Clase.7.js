623d5ad380722dad81fbc6c0d9fa1f0076276020
agusces0 <agusces0@gmail.com>
2023-06-16 00:23:31 -0300

    Clase 7 parte 2

--- javascript/semana7/Clase.7.js
+++ javascript/semana7/Clase.7.js
@@ -36,4 +36,41 @@ class Persona{ //Clase Padre
 
     
 }
+class Empleado extends Persona{ //Clase hija
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido); 
+        this._departamento= departamento; 
 
+    }
+    get (departamento){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento; 
+    }
+
+    //Sobreescritura 
+    nombreCompleto(){
+        return super.nombreCompleto()+', ' +this._departamento;
+    }
+
+}
+let persona1 = new Persona('Martin', 'Perez');
+console.log(persona1.nombre);
+persona1.nombre="Juan Carlos"
+console.log(persona1.nombre);
+//console.log(persona1)
+let persona2 = new Persona('Carlos', 'Lara'); 
+console.log(persona2.nombre);
+persona2.nombre="Maria Laura";
+console.log(persona2.nombre); 
+//console.log(persona2); 
+
+let Empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
+console.log(Empleado1); 
+console.log(Empleado1.nombreCompleto());  
+
+//object.prototype.toString Esta es la manerda de acceder a los atributos y metodos de manera dinamica
+console.log(Empleado1.toString()); 
+console.log(persona1.toString()); 
+console.log(persona2.toString()); 
