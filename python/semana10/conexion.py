bc1c36943b13f9609db54afb893f240f2e02b3a0
LucasM2003 <111904619+LucasM2003@users.noreply.github.com>
2023-06-28 17:46:54 -0300

    10.2 Obtener una conexión a partir del Pool: Parte 1 y 2

---
+++ python/semana10/conexion.py
@@ -0,0 +1,25 @@
+#Parte 1- Lucas Montoya
+from psycopg2 import pool
+
+from logger_base import log
+import sys
+
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        conexion = cls.obtenerPool().getconn()
+        log.debug(f'Conexión obtenida del pool: {conexion}')
+        return conexion
+
+    @classmethod
+    def obtenerCursor(cls):
+        pass
