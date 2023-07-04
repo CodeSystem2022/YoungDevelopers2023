daab6d78e4c8ee494bb60b1e9e7b6575f5f99594
valemorbi <113058574+valemorbi@users.noreply.github.com>
2023-07-04 15:20:17 -0300

    Diferentes Querys en MySQL -> Parte 1 y 2

--- java/semana11/estudiantes.sql
+++ java/semana11/estudiantes.sql
@@ -3,3 +3,10 @@
 SELECT * FROM estudiantes2022;
 -- Insertar estudiante
 INSERT INTO estudiantes2022 (nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "260444664", "Juan@gmail.com");
+UPDATE estudiantes2022 SET nombre="Juan Carlos", apellido="Garcia" WHERE idestudiantes2022= 1;
+-- Delate(eliminar)
+
+DELETE FROM estudantes2022 WHERE idestuantes2022=3;
+
+-- Para modificar el id estudiantes2022 y comience en 1
+ALTER TABLE estudiantes2022 AUTO_INCREMENT = 1;
