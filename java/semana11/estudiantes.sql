b2f1c0185977744830eb13524597995f43e7a966
LucasM2003 <111904619+LucasM2003@users.noreply.github.com>
2023-07-04 15:17:53 -0300

    Diferentes Querys en MySQL -> Parte 1 y 2

---
+++ java/semana11/estudiantes.sql
@@ -0,0 +1,5 @@
+-- Comenzamos con CRUD: create(insertar), reed(leer), update(actualizar)
+-- Listar los estudiantes (read)
+SELECT * FROM estudiantes2022;
+-- Insertar estudiante
+INSERT INTO estudiantes2022 (nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "260444664", "Juan@gmail.com");
