f7a3c9b2f69c8291b234407d3a7c458c228abf9c
ana-zn <113066266+ana-zn@users.noreply.github.com>
2023-07-05 22:38:11 -0300

    CalculadoraUTN parte2

--- java/semana9/CalculadoraUTN.java
+++ java/semana9/CalculadoraUTN.java
@@ -30,4 +30,39 @@ public class CalculadoraUTN {
                             resultado = operando1 + operando2;
                             System.out.println("Resultado de la suma: " + resultado);
                         }
+                        case 2 -> { //Resta
+                            resultado = operando1 - operando2;
+                            System.out.println("Resultado de la resta: " + resultado);
+                        }
+                        case 3 -> { //Multiplicacion
+                            resultado = operando1 * operando2;
+                            System.out.println("Resultado de la multiplicacion: " + resultado);
+                        }
+                        case 4 -> { //Division
+                            resultado = operando1 / operando2;
+                            System.out.println("Resultado de la division: " + resultado);
+                        }
+                        default -> System.out.println("Opcion erronea: " + operacion);
+                    } //Fin switch
+
+                } //Fin del if
+                else if (operacion >= 5) {
+                    System.out.println("Hasta pronto...");
+                    break; //Rompe el ciclo y sale
+                } else {
+                    System.out.println("Opcion erronea: " + operacion);
+                }
+                //Imprimimos un salto de linea antes de repetir el menú
+                System.out.println();
+            } catch(Exception e ){ //Fin del try comienzo del catch
+                System.out.println("Ocurrio un error: "+e.getMessage());
+                System.out.println();
+            }//Fin catch
+    }//Fin while
+    }//Fin main
+}//Fin clase
+
+
+
+                            
 
